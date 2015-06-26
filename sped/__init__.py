0fa8e1c5157ad89e4af95874e667ceee6d0f243c
Sergio Garcia <sergio@ginx.com.br>
2015-06-25 22:44:36 -0300

    Adicionado suporte a tabelas do SPED

--- sped/__init__.py
+++ sped/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 
 
-__version__ = '0.2.dev7'
+__version__ = '0.2.dev8'
---
+++ sped/ecd/tabelas.py
@@ -0,0 +1,30 @@
+__author__ = 'sergio'
+
+import os
+import re
+
+__all__ = ['PLANO_REFERENCIAL_PJ_RESUMIDO']
+
+PLANO_REFERENCIAL_PJ_RESUMIDO = []
+
+path_tabelas = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'tabelas')
+# tabela = 'SPEDCONTABIL_DINAMICO_2014$SPEDECF_DINAMICA_P100$12$389'
+tabela = 'SPEDCONTABIL_DINAMICO_2014$SPEDECF_DINAMICA_P150$3$398'
+path_tabela = os.path.join(path_tabelas, tabela)
+
+def carregar_tabela(caminho_tabela, encoding='cp1252'):
+    print('Loading: ' + caminho_tabela)
+
+    with open(caminho_tabela, 'r', encoding=encoding) as file:
+        header = re.match('versão=(?P<version>\\d+) (?P<columns>.+)$', file.readline())
+        if not header:
+            raise Error()
+        version = header.groupdict()['version']
+        columns = header.groupdict()['columns'].split(', ')
+
+        return [dict(zip(columns, line.split('|'))) for line in file.readlines()]
+
+if not PLANO_REFERENCIAL_PJ_RESUMIDO:
+    pj1 = carregar_tabela(os.path.join(path_tabelas, 'SPEDCONTABIL_DINAMICO_2014$SPEDECF_DINAMICA_P100$12$389'))
+    pj2 = carregar_tabela(os.path.join(path_tabelas, 'SPEDCONTABIL_DINAMICO_2014$SPEDECF_DINAMICA_P150$3$398'))
+    PLANO_REFERENCIAL_PJ_RESUMIDO = pj1 + pj2
