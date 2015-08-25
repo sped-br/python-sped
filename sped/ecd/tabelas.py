ed40b6c312bff5a1879f41d13b2ca3c195f0ef73
Sergio Garcia <s.garcia@cinq.com.br>
2015-08-25 13:44:30 -0300

    Encoding errors.

--- sped/ecd/tabelas.py
+++ sped/ecd/tabelas.py
@@ -1,4 +1,4 @@
-__author__ = 'sergio'
+# -*- coding: utf-8 -*-
 
 import os
 import re
@@ -7,16 +7,16 @@ __all__ = ['PLANO_REFERENCIAL_PJ_RESUMIDO']
 
 PLANO_REFERENCIAL_PJ_RESUMIDO = []
 
-path_tabelas = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'tabelas')
+path_tabelas = os.path.join(os.path.dirname(os.path.realpath(__file__)), u'tabelas')
 # tabela = 'SPEDCONTABIL_DINAMICO_2014$SPEDECF_DINAMICA_P100$12$389'
-tabela = 'SPEDCONTABIL_DINAMICO_2014$SPEDECF_DINAMICA_P150$3$398'
+tabela = u'SPEDCONTABIL_DINAMICO_2014$SPEDECF_DINAMICA_P150$3$398'
 path_tabela = os.path.join(path_tabelas, tabela)
 
 def carregar_tabela(caminho_tabela, encoding='cp1252'):
     print('Loading: ' + caminho_tabela)
 
     with open(caminho_tabela, 'r', encoding=encoding) as file:
-        header = re.match('versão=(?P<version>\\d+) (?P<columns>.+)$', file.readline())
+        header = re.match(u'versão=(?P<version>\\d+) (?P<columns>.+)$', file.readline())
         if not header:
             raise Error()
         version = header.groupdict()['version']
@@ -25,6 +25,6 @@ def carregar_tabela(caminho_tabela, encoding='cp1252'):
         return [dict(zip(columns, line.split('|'))) for line in file.readlines()]
 
 if not PLANO_REFERENCIAL_PJ_RESUMIDO:
-    pj1 = carregar_tabela(os.path.join(path_tabelas, 'SPEDCONTABIL_DINAMICO_2014$SPEDECF_DINAMICA_P100$12$389'))
-    pj2 = carregar_tabela(os.path.join(path_tabelas, 'SPEDCONTABIL_DINAMICO_2014$SPEDECF_DINAMICA_P150$3$398'))
+    pj1 = carregar_tabela(os.path.join(path_tabelas, u'SPEDCONTABIL_DINAMICO_2014$SPEDECF_DINAMICA_P100$12$389'))
+    pj2 = carregar_tabela(os.path.join(path_tabelas, u'SPEDCONTABIL_DINAMICO_2014$SPEDECF_DINAMICA_P150$3$398'))
     PLANO_REFERENCIAL_PJ_RESUMIDO = pj1 + pj2
