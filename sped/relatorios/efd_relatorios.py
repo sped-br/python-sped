d10c9b29332344a114b78880b215e1391315b27d
claudiofsr <claudiofsr@yahoo.com>
2020-03-01 19:18:11 -0300

    imprime relatórios de SPED EFD em Excel

--- sped/relatorios/efd_relatorios.py
+++ sped/relatorios/efd_relatorios.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 Autor = 'Claudio Fernandes de Souza Rodrigues (claudiofsr@yahoo.com)'
-Data  = '29 de Fevereiro de 2020 (início: 29 de Janeiro de 2020)'
+Data  = '01 de Março de 2020 (início: 29 de Janeiro de 2020)'
 
 # Instruções (no Linux):
 # Digite em seu web brawser o endereço abaixo:
@@ -32,8 +32,9 @@ Data  = '29 de Fevereiro de 2020 (início: 29 de Janeiro de 2020)'
 import sys, os
 from time import time, sleep
 from sped import __version__
-from sped.relatorios.efd_read_dir import ReadFiles, Total_Execution_Time
-from sped.relatorios.efd_info import SPED_EFD_Info
+from sped.relatorios.find_efd_files import ReadFiles, Total_Execution_Time
+from sped.relatorios.print_csv_file import SPED_EFD_Info
+from sped.relatorios.convert_csv_to_xlsx import CSV_to_Excel
 
 # Versão mínima exigida: python 3.6.0
 python_version = sys.version_info
@@ -44,7 +45,7 @@ if python_version < (3,6,0):
 
 def main():
 
-	print(f'\nPython Sped - versão: {__version__}\n')
+	print(f'\n\tPython Sped - versão: {__version__}\n')
 	
 	dir_path = os.getcwd() # CurrentDirectory
 	extensao = 'txt'
@@ -76,34 +77,39 @@ def main():
 	elif len(arquivos_sped_efd) == 1:
 		indice_do_arquivo = 0
 	else:
-		# https://stackoverflow.com/questions/8384737/extract-file-name-from-path-no-matter-what-the-os-path-format
-		basename = os.path.basename(__file__) # 'efd_relatorios'
-		file_ext = os.path.splitext(__file__)[-1] # ('./efd_relatorios', '.py')
-		executavel = basename.replace(file_ext, '')
-		
-		print(f"Execute este programa no diretório que contenha os arquivos SPED EFD.")
-		print(f"Na linha de comando, digite:\n")
-		print(f"\t{executavel}\n")
+		print(f"\tDiretório atual: '{dir_path}'.")
+		print(f"\tNão foi encontrado arquivos SPED EFD neste diretório.\n")
 		exit()
 
 	# arquivo SPED EFD
 	file_path   = arquivos_sped_efd[indice_do_arquivo]
 	tipo_da_efd = lista_de_arquivos.informations[file_path]['tipo']
 	codificacao = lista_de_arquivos.informations[file_path]['codificação']
+
+	filename = os.path.splitext(file_path)[0] # ('./efd_info', '.py')
+	arquivo_csv   = filename + '.csv'
+	arquivo_excel = filename + '.xlsx'
 	
-	print(f"\nFoi selecionado o arquivo {indice_do_arquivo + 1}: '{file_path}'\n")
-	input("Tecle Enter para gerar arquivo .csv com informações da EFD ")
+	print(f"\nSelecionado o arquivo {indice_do_arquivo + 1}: '{file_path}'.\n")
+	input("Tecle Enter para gerar arquivo Excel .xlsx com informações da SPED EFD.")
 	print()
 	
 	start = time()
 	
-	efd = SPED_EFD_Info(file_path, encoding=codificacao, efd_tipo = tipo_da_efd, verbose=False)
+	csv_file = SPED_EFD_Info(file_path, encoding=codificacao, efd_tipo = tipo_da_efd, verbose=False)
 	
-	efd.imprimir_informacoes
+	csv_file.imprimir_arquivo_csv
+
+	excel_file = CSV_to_Excel(arquivo_csv, arquivo_excel, verbose=False)
+
+	excel_file.convert_csv_to_xlsx
+
+	if os.path.exists(arquivo_csv):
+		os.remove(arquivo_csv)
 
 	end = time()
 	
-	print(f'\nTotal Execution Time: {Total_Execution_Time(start,end)} \n')
+	print(f'\nTotal Execution Time: {Total_Execution_Time(start,end)}\n')
 
 if __name__ == '__main__':
 	main()
