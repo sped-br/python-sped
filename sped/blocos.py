d493836523af0981a8df5ba2efd1bc9c09ccecfe
claudiofsr <claudiofsr@yahoo.com>
2020-03-02 13:53:23 -0300

    reintrodução de comentários

--- sped/blocos.py
+++ sped/blocos.py
@@ -6,6 +6,8 @@ class Bloco(object):
     def __init__(self, nome=''):
         self._nome = nome
         self._registros = []
+        #self.registro_abertura = Registro()
+        #self.registro_encerramento = Registro()
 
     def __repr__(self):
         return '<%s.%s(%s)>' % (self.__class__.__module__,
@@ -13,10 +15,12 @@ class Bloco(object):
 
     @property
     def abertura(self):
+        # Define o indicador de movimento ou dados
         return self.registro_abertura
 
     @property
     def encerramento(self):
+        # Define a quantidade de registros
         return self.registro_encerramento
 
     @property
@@ -24,4 +28,5 @@ class Bloco(object):
         return [self.abertura] + self._registros + [self.encerramento]
 
     def add(self, registro):
+        # Não adiciona o registro de abertura e fechamento
         self._registros.append(registro)
